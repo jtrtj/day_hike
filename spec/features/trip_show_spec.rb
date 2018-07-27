0cdb79d62043b68b5d7127912ef66a66a0ed9eef
jtrtj <jt.r@live.com>
2018-07-27 10:32:30 -0600

    tests green

--- spec/features/trip_show_spec.rb
+++ spec/features/trip_show_spec.rb
@@ -12,5 +12,14 @@ describe 'a visitor' do
       expect(page).to have_content(trail_1.length)
       expect(page).to have_content(trail_1.address)
     end
+    it 'will see a list of trails and their name will be a link' do
+      trip_1 = Trip.create!(name: 'gasdf')
+      trail_1 = trip_1.trails.create!(length: 654, name: "sfdg", address: "gfad")
+
+      visit trip_path(trip_1)
+      click_on trail_1.name
+
+      expect(current_path).to eq(trail_path(trail_1))
+    end
   end
 end
